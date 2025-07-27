70ca319153f43074be3d78d84ffe5cc6c714b887
zenonet <git@zenonet.de>
2025-07-27 17:14:21 +0200

    Added frontend with movie list with search

---
+++ frontend/src/MovieList.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+
+    class Movie{
+        name!: string;
+        id!: string;
+    }
+
+    let { onMovieClicked } = $props<{ onMovieClicked: (id:Movie) => void}>();
+
+    let searchVal = $state("");
+
+    let movies: Movie[] = $state([]);
+
+    async function fetchMovies(){
+        console.log(searchVal)
+        const resp = await fetch(`http://localhost:2222/movie?page=0&per_page=50&name=${encodeURI(searchVal)}`);
+        movies = await resp.json();
+    }
+
+    fetchMovies();
+
+</script>
+
+<div>
+    <input placeholder="Search..." bind:value={searchVal} oninput={fetchMovies}>
+
+    <div class="movie-list">
+        {#each movies as movie}
+            <button class="movie-list-entry" onclick={onMovieClicked(movie)}>
+                { movie.name }
+            </button>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .movie-list-entry{
+        background: var(--background);
+
+        border: none;
+		text-align: left;
+    }
+
+    .movie-list{
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+
+        gap: 10px;
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import MovieList from "../MovieList.svelte";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<MovieList onMovieClicked={(movie) => console.log(`${movie.name} clicked`)}/>
