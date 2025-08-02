431ab7550aea42e8ebef3f8562710584067371e2
zenonet <git@zenonet.de>
2025-08-02 20:35:16 +0200

    Removed svelte / page default content

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,11 +6,6 @@
     let selectedId = $state("");
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
 <div class="side-by-side-layout">
     <MovieList
         onMovieClicked={(movie) => selectedId = movie.id}
