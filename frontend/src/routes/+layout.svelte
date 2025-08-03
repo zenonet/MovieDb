75f3e7931f5af34d8da611dff8a813b2e8e3e4fb
zenonet <git@zenonet.de>
2025-08-03 16:46:13 +0200

    Added navigation bar

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let {children} = $props();
+</script>
+
+<nav>
+    <a href="/">Movies</a>
+    <a href="/watchlist">Watchlists</a>
+</nav>
+
+{@render children()}
+
+
+<style>
+    nav{
+        padding: 15px;
+        display: flex;
+        gap: 10px;
+    }
+</style>
